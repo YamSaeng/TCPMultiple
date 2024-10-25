5ca9ec1d127684eca01cda7066891d5e3579ce44
성원 정 <tyhg56@naver.com>
2024-10-25 19:40:45 +0900

    docs : " Config 수정 "
    Config
     - USER_DB 상수값 생성

--- src/config/Config.ts
+++ src/config/Config.ts
@@ -1,4 +1,4 @@
-import { PORT, HOST, CLIENT_VERSION } from "../constants/Env.js";
+import { PORT, HOST, CLIENT_VERSION, DB1_NAME, DB1_USER, DB1_PASSWORD, DB1_HOST, DB1_PORT } from "../constants/Env.js";
 
 export const config = {
     gameserver: {
@@ -7,5 +7,14 @@ export const config = {
     },
     client: {
         version: CLIENT_VERSION
+    },
+    databases:{
+        USER_DB:{
+            name: DB1_NAME,
+            user: DB1_USER,
+            password: DB1_PASSWORD,
+            host: DB1_HOST,
+            port:DB1_PORT
+        }
     }
 }
