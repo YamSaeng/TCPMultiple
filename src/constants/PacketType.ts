6228d0861a781f3c40c3bbecc067eb829c54d4ab
성원 정 <tyhg56@naver.com>
2024-10-26 19:05:44 +0900

    Feat : " PacketType 추가 "
    PacketType
     - 서버에서 사용하는 패킷 타입 상수값으로 정의
     - PACKET_TYPE_PING ( 핑 패킷 )
     - PACKET_TYPE_NORMAL ( 일반 패킷 )
     - PACKET_TYPE_GAME_START ( 게임 시작 패킷 )
     - PACKET_TYPE_LOCATION ( 위치 업데이트 패킷 )

---
+++ src/constants/PacketType.ts
@@ -0,0 +1,9 @@
+export const TOTAL_LENGTH = 4;
+export const PACKET_TYPE_LENGTH = 1;
+
+export const PACKET_TYPE = {
+    PACKET_TYPE_PING: 0,
+    PACKET_TYPE_NORMAL: 1,
+    PACKET_TYPE_GAME_START: 2,
+    PACKET_TYPE_LOCATION: 3
+};
