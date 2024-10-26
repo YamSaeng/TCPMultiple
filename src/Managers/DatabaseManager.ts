a0b772103ef6e330e84246c2a16a6af4e871aedb
성원 정 <tyhg56@naver.com>
2024-10-26 19:07:40 +0900

    Fix : " DatabaseManager 수정 "
    DatabaseManager
     - FindUserByDeviceID 추가 ( DeviceID로 유저 찾기 쿼리 실행 함수 )
     - CreateUser 추가 ( DB에 유저 저장 함수 )
     - UpdateUserLogin 추가 ( 유저 접속 로그인 시간 업데이트 함수 )

--- src/Managers/DatabaseManager.ts
+++ src/Managers/DatabaseManager.ts
@@ -1,13 +1,15 @@
 import fs from "fs";
 import path from 'path';
+import { v4 as uuidv4 } from 'uuid';
 import mysql, { FieldPacket, QueryResult } from "mysql2/promise";
 import FileParser from "../FileParser.js";
 import { FormatDate } from "../utils/DateFormatter.js";
 import { config } from "../config/Config.js";
+import { USER_SQL_QUERIES } from "../utils/db/user/UserQueries.js";
 
 class DatabaseManager {
     private static gInstance: any = null;
-    public pools : { [key: string]: any } = {};
+    public pools: { [key: string]: any } = {};
 
     static GetInstance() {
         if (DatabaseManager.gInstance == null) {
@@ -19,25 +21,25 @@ class DatabaseManager {
 
     CreatePool(poolName: string, dbConfig: any) {
         const pool = mysql.createPool({
-           host:dbConfig.host,
-           port:dbConfig.port,
-           user:dbConfig.user,
-           password: dbConfig.password,
-           database: dbConfig.name,
-           waitForConnections:true,
-           connectionLimit:10,
-           queueLimit:0 
+            host: dbConfig.host,
+            port: dbConfig.port,
+            user: dbConfig.user,
+            password: dbConfig.password,
+            database: dbConfig.name,
+            waitForConnections: true,
+            connectionLimit: 10,
+            queueLimit: 0
         });
 
         const originalQuery = pool.query;
 
         pool.query = async <T extends QueryResult>(sql: any, params?: any): Promise<[T, FieldPacket[]]> => {
-            const date = new Date();
             // 쿼리 실행시 로그
-            console.log(
-                `[${FormatDate(date)}] Executing query: ${sql} ${params ? `, ${JSON.stringify(params)}` : ``
-                }`,
-            );
+            // const date = new Date();            
+            // console.log(
+            //     `[${FormatDate(date)}] Executing query: ${sql} ${params ? `, ${JSON.stringify(params)}` : ``
+            //     }`,
+            // );
 
             const result = await originalQuery.call(pool, sql, params);
             return result as [T, FieldPacket[]];
@@ -46,40 +48,52 @@ class DatabaseManager {
         this.pools[poolName] = pool;
     }
 
-    async ExecuteSqlFile(pool : any, filePath: string)
-    {
+    async ExecuteSqlFile(pool: any, filePath: string) {
         const sql = fs.readFileSync(filePath, "utf-8");
 
         const queries = sql
             .split(";")
-            .map((query:string)=>query.trim())
-            .filter((query:string)=>query.length>0);
+            .map((query: string) => query.trim())
+            .filter((query: string) => query.length > 0);
 
-        for(const query of queries)
-        {
+        for (const query of queries) {
             await pool.query(query);
         }
     }
 
-    async CreateSchemas()
-    {
+    async CreateSchemas() {
         const sqlDir = FileParser.GetInstance().GetDir("db/sql");
-        try{
-            await this.ExecuteSqlFile(this.pools["USER_DB"], path.join(sqlDir,"userDB.sql"));
+        try {
+            await this.ExecuteSqlFile(this.pools["USER_DB"], path.join(sqlDir, "userDB.sql"));
 
-            console.log("데이터베이스 테이블생이 성공적으로 생성되었습니다.");
+            console.log("데이터베이스 테이블이 성공적으로 생성되었습니다.");
         }
-        catch(error)
-        {
+        catch (error) {
             console.error("데이터베이스 테이블 생성 중 오류가 발생했습니다.", error);
         }
-    }       
+    }
+
+    async FindUserByDeviceID(deviceID: string) {
+        const [rows] = await this.pools["USER_DB"].query(USER_SQL_QUERIES.FIND_USER_BY_DEVICE_ID, [deviceID]);
+        return rows[0];
+    }
+
+    async CreateUser(deviceID: string){
+        const id = uuidv4();
+        await this.pools["USER_DB"].query(USER_SQL_QUERIES.CREATE_USER, [id,deviceID]);
+
+        return { id, deviceID };
+    }
+
+    async UpdateUserLogin(id:string){
+        await this.pools["USER_DB"].query(USER_SQL_QUERIES.UPDATE_USER_LOGIN, [id]);
+    }
 
     private constructor() {
         const { databases } = config;
 
-        this.CreatePool("USER_DB",databases.USER_DB);
-    }    
+        this.CreatePool("USER_DB", databases.USER_DB);
+    }
 }
 
 export default DatabaseManager;
