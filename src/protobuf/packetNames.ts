03bd8fbcb30d7300ea3f3213d7b57e2fef25cda9
성원 정 <tyhg56@naver.com>
2024-10-27 18:36:00 +0900

    docs : " packetNames 수정 "
    packetNames
     - common 패킷 이름 추가
     - response 패킷 이름 추가

--- src/protobuf/packetNames.ts
+++ src/protobuf/packetNames.ts
@@ -1,5 +1,12 @@
 export const packetNames = {
+    common: {
+        Packet: "common.Packet",
+        Ping: "common.Ping"
+    },
     gameInitial: {
         GameInitialPacket: "gameInitial.GameInitialPacket"
+    },
+    response: {
+        Response: "response.Response"
     }
 }
