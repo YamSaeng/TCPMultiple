ca1a52f8f01f5a1acda86b383a0b50cc26d9cdce
성원 정 <tyhg56@naver.com>
2024-10-29 01:08:56 +0900

    Fix : " OnEnd 수정 "
    OnEnd
     - 클라 접속 종료 하면 유저 제거

--- src/events/OnEnd.ts
+++ src/events/OnEnd.ts
@@ -1,4 +1,7 @@
+import GameServer from "../server/Server.js";
 
 export const OnEnd = (socket: any) => () => {
     console.log("클라 연결 종료", socket.remoteAddress, socket.remotePort);
+
+    GameServer.GetInstance().RemoveUser(socket);    
 }
