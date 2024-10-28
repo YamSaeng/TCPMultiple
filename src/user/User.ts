76bdae49fdc5d46a429710ba730738ee0bd2eed8
성원 정 <tyhg56@naver.com>
2024-10-28 18:54:01 +0900

    Fix : " User 수정 "
    User
     - private 변수 get, set 함수 추가

--- src/user/User.ts
+++ src/user/User.ts
@@ -1,35 +1,72 @@
 import { CreatePingPacket } from "../server/packet/MakePacket.js";
 
 class User {
-    private id : number;
-    private socket : any;
-    private x : number;
-    private y : number;    
-    private lastUpdateTime : number;
-    private latency : number;
+    private id: number;
+    private playerId: number;
+    private socket: any;
+    private x: number;
+    private y: number;
+    private lastUpdateTime: number;
+    private latency: number;
 
-    constructor(id : number, socket : any) {
+    constructor(id: number, socket: any) {
         this.id = id;
+        this.playerId = 0;
         this.socket = socket;
         this.x = 0;
-        this.y = 0;        
+        this.y = 0;
         this.lastUpdateTime = Date.now();
         this.latency = 0;
     }
 
-    Ping(){
+    Ping() {
         const now = Date.now();
 
         console.log(`${this.id}: ping`);
         this.socket.write(CreatePingPacket(now));
     }
 
-    HandlePong(data: any)
-    {
+    HandlePong(data: any) {
         const now = Date.now();
 
         this.latency = (now - data.timestamp) / 2;
         console.log(`Receive pong user ${this.id} at ${now} with latency ${this.latency}ms`);
+    }
+
+    GetPlayerId()
+    {
+        return this.playerId;
+    }
+
+    SetPlayerId(playerId : number)
+    {
+        this.playerId = playerId;
+    }    
+
+    GetLatency()
+    {
+        return this.latency;
+    }
+
+    SetLatency(latency : number)
+    {
+        this.latency = latency;
+    }
+
+    GetPositionX()
+    {
+        return this.x;
+    }
+
+    GetPositionY()
+    {
+        return this.y;
+    }
+
+    UpdatePosition(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+        this.lastUpdateTime = Date.now();
     }
 }
 
