d8802e8f852b10cb8265c53583b337b4cf3e3a80
성원 정 <tyhg56@naver.com>
2024-10-28 21:19:27 +0900

    Fix : " GameInitialHandler 수정 "
    GameInitialHandler
     - 클라가 전달한 playerId와 latency 저장

--- src/server/handlers/GameInitialHandler.ts
+++ src/server/handlers/GameInitialHandler.ts
@@ -5,7 +5,9 @@ import GameServer from "../Server.js";
 
 const GameInitialHandler = async ({ socket, userId, payload }: { socket: any; userId: any; payload: any; }) => {
     try {
-        const { deviceId } = payload;
+        const { deviceId, playerId, latency } = payload;
+
+        console.log(`deviceID ${deviceId} playerId ${playerId} latency ${latency}`);        
 
         // DB에 접속한 대상이 저장되어 있는지 확인
         let user = await DatabaseManager.GetInstance().FindUserByDeviceId(deviceId);
@@ -16,10 +18,19 @@ const GameInitialHandler = async ({ socket, userId, payload }: { socket: any; us
         else {
             // 저장 되어 있으면 Login 시간 업데이트
             await DatabaseManager.GetInstance().UpdateUserLogin(user.id);
-        }
+        }               
 
         // 새로 들어온 유저를 저장
-        GameServer.GetInstance().AddUser(socket, user.id);
+        const gameserverUser = GameServer.GetInstance().AddUser(deviceId, socket);
+        if(!gameserverUser)
+        {
+            console.log("gameserverUser가 생성되지 않았습니다.");
+        }        
+
+        gameserverUser.SetPlayerId(playerId);
+        gameserverUser.SetLatency(latency);
+
+        // 이미 들어와 있는 유저들에게 새로 들어온 유저를 생성하라고 알려줌
 
         const gameInitialResponse = CreateResponse(
             HANDLER_IDS.INITIAL,
