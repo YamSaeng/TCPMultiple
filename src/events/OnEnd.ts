7385d13517dcc75095df2ff2ff7d29dc642de545
성원 정 <tyhg56@naver.com>
2024-10-30 01:27:03 +0900

    Fix : " OnEnd 수정 "
    OnEnd
     - 서버 접속 종료한 유저 마지막 위치 좌표값 가져와서 DB에 저장

--- src/events/OnEnd.ts
+++ src/events/OnEnd.ts
@@ -1,7 +1,12 @@
+import DatabaseManager from "../Managers/DatabaseManager.js";
 import GameServer from "../server/Server.js";
 
-export const OnEnd = (socket: any) => () => {
-    console.log("클라 연결 종료", socket.remoteAddress, socket.remotePort);
+export const OnEnd = (socket: any) => () => {    
+    console.log(`클라 연결 종료 ${socket.remoteAddress} ${socket.remotePort}`);
 
-    GameServer.GetInstance().RemoveUser(socket);    
+    const endUser = GameServer.GetInstance().GetUserBySocket(socket);    
+
+    DatabaseManager.GetInstance().RemoveUser(endUser);
+
+    GameServer.GetInstance().RemoveUser(socket);
 }
