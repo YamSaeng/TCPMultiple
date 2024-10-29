ffc408c6575ad01bde920ee58eb239b39186ce70
성원 정 <tyhg56@naver.com>
2024-10-30 01:24:08 +0900

    docs : " UserQueries 수정 "
    UserQueries
     - REMOVE_USER 쿼리 추가 ( 클라 접속 종료시 위치 값 저장 )

--- src/utils/db/user/UserQueries.ts
+++ src/utils/db/user/UserQueries.ts
@@ -1,8 +1,10 @@
-export const USER_SQL_QUERIES ={
+export const USER_SQL_QUERIES = {
 
     FIND_USER_BY_DEVICE_ID: "SELECT * FROM user WHERE device_id = ?",
 
     CREATE_USER: "INSERT INTO user (id, device_id) VALUES (?,?)",
+
+    REMOVE_USER: "UPDATE user SET x = ?, y = ? WHERE device_id = ?",
 
     UPDATE_USER_LOGIN: "UPDATE user SET last_login = CURRENT_TIMESTAMP WHERE id = ?"
 }
