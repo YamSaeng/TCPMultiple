2a88ab245f652698da8f64b6b4de0844d693282a
성원 정 <tyhg56@naver.com>
2024-10-31 18:43:15 +0900

    Fix : " User 수정 "
    User
     - PongCount 추가 ( 핑퐁 패킷 응답 확인 갯수 )
     - Ping() 삭제 ( 서버에서 핑 패킷 보내주도록 수정 )

--- src/user/User.ts
+++ src/user/User.ts
@@ -1,4 +1,5 @@
 import { CreatePingPacket } from "../server/packet/MakePacket.js";
+import GameServer from "../server/Server.js";
 
 class User {
     private id: number;
@@ -8,6 +9,7 @@ class User {
     private y: number;
     private lastUpdateTime: number;
     private latency: number;
+    public pongCount: number;
 
     constructor(id: number, socket: any) {
         this.id = id;
@@ -17,20 +19,26 @@ class User {
         this.y = 0;
         this.lastUpdateTime = Date.now();
         this.latency = 0;
-    }
-
-    Ping() {
-        const now = Date.now();
-
-        console.log(`${this.id}: ping`);
-        this.socket.write(CreatePingPacket(now));
+        this.pongCount = 0;
     }
 
     HandlePong(data: any) {
         const now = Date.now();
 
         this.latency = (now - data.timestamp) / 2;
-        console.log(`Receive pong user ${this.id} at ${now} with latency ${this.latency}ms`);
+        console.log(`Receive pong user ${this.id} latency ${this.latency}ms`);
+
+        this.pongCount = 0;
+    }
+
+    GetId()
+    {
+        return this.id;
+    }
+
+    GetSocket()
+    {
+        return this.socket;
     }
 
     GetPlayerId()
@@ -58,9 +66,19 @@ class User {
         return this.x;
     }
 
+    SetPositionX(x : number)
+    {
+        this.x = x;
+    }    
+
     GetPositionY()
     {
         return this.y;
+    }
+
+    SetPositionY(y: number)
+    {
+        this.y = y;
     }
 
     UpdatePosition(x: number, y: number) {
