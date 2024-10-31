7c66ffd1ec9b1afe951bd511bde95d5255e2f961
성원 정 <tyhg56@naver.com>
2024-10-31 18:38:17 +0900

    docs : " Config 수정 "
    Config, Env 수정
     - PONG_MAX_COUNT 추가
      - pong packet 최대 갯수
      - 최대 갯수 이상 넘어 가면 해당 클라의 연결을 끊음

--- src/config/Config.ts
+++ src/config/Config.ts
@@ -1,10 +1,11 @@
-import { PORT, HOST, CLIENT_VERSION, DB1_NAME, DB1_USER, DB1_PASSWORD, DB1_HOST, DB1_PORT } from "../constants/Env.js";
+import { PORT, HOST, CLIENT_VERSION, DB1_NAME, DB1_USER, DB1_PASSWORD, DB1_HOST, DB1_PORT, PONG_MAX_COUNT } from "../constants/Env.js";
 import { TOTAL_LENGTH, PACKET_TYPE_LENGTH } from "../constants/PacketType.js";
 
 export const config = {
     gameserver: {
         port: PORT,
-        host: HOST
+        host: HOST,
+        pongCount: PONG_MAX_COUNT
     },
     client: {
         version: CLIENT_VERSION
