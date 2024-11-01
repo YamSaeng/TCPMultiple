1b86719c80033e5d11c623184f6152ea65f87f7a
성원 정 <tyhg56@naver.com>
2024-11-01 14:25:32 +0900

    Fix : " OnError 수정 "
    OnError
     - 에러 발생한 User 뒷 정리

--- src/events/OnError.ts
+++ src/events/OnError.ts
@@ -1,4 +1,12 @@
+import DatabaseManager from "../Managers/DatabaseManager.js";
+import GameServer from "../server/Server.js";
 
 export const OnError = (socket: any) => (error:any) => {
-    console.error("소켓 에러 발생", error);
+     // 접속이 끊긴 유저를 가져옴
+     const endUser = GameServer.GetInstance().GetUserBySocket(socket);    
+     // DB에 마지막 위치값 저장
+     DatabaseManager.GetInstance().RemoveUser(endUser);
+ 
+     // 서버에서 유저 삭제
+     GameServer.GetInstance().RemoveUser(socket);
 }
