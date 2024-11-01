786a1fdf0906dccebaff997b3200fcbc60f9d00c
성원 정 <tyhg56@naver.com>
2024-11-01 10:39:43 +0900

    Comment : " OnEnd 수정 "
    OnEnd
     - 주석 달기

--- src/events/OnEnd.ts
+++ src/events/OnEnd.ts
@@ -4,9 +4,11 @@ import GameServer from "../server/Server.js";
 export const OnEnd = (socket: any) => () => {    
     console.log(`클라 연결 종료 ${socket.remoteAddress} ${socket.remotePort}`);
 
+    // 접속이 끊긴 유저를 가져옴
     const endUser = GameServer.GetInstance().GetUserBySocket(socket);    
-
+    // DB에 마지막 위치값 저장
     DatabaseManager.GetInstance().RemoveUser(endUser);
 
+    // 서버에서 유저 삭제
     GameServer.GetInstance().RemoveUser(socket);
 }
